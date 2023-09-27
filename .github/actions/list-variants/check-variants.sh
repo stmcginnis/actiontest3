83e3fb1619b3ae72623e396e37ba71a616e0bfe8
Sean McGinnis <stmcg@amazon.com>
2023-09-27 21:13:03 +0000

    Next

--- .github/actions/list-variants/check-variants.sh
+++ .github/actions/list-variants/check-variants.sh
@@ -71,12 +71,12 @@ if [[ "${filter}" != "false" ]]; then
         done <"/tmp/variant-info/${variant}"
     done
     #output=$(IFS=, echo "variants=[${variants[*]}]")
-    output="variants=["
-    for variant in ${variants}; do
-        output+="\"${variant}\","
-    done
-    output=${output::-1}
-    output+="]"
+    output="variants=[$(printf '"%s",'  ${variants[@]})]"
+    #for variant in ${variants}; do
+    #    output+="\"${variant}\","
+    #done
+    #output=${output::-1}
+    #output+="]"
 else
     cd variants
     output="variants=$(ls -d */ | cut -d'/' -f 1 | grep -vE '^(shared|target)$' | jq -R -s -c 'split("\n")[:-1]')"
